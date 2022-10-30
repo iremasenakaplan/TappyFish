aa203d62b0536ce6106de8946d4f166c783aed97
iremasenakaplan <iremasenakaplan19@gmail.com>
2022-10-30 19:08:38 +0300

    Fish, obstacle and ground move

--- Assets/Script/Fish.cs
+++ Assets/Script/Fish.cs
@@ -5,14 +5,50 @@ using UnityEngine;
 public class Fish : MonoBehaviour
 {
     Rigidbody2D _rb; // degisken tanimlandi
+    [SerializeField]
+    private float _speed;
+    int angle;
+    int maxAngle = 20;
+    int minAngle =-60;
+
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>(); // Rigidbody2d component ini aktardik
-        _rb.velocity = new Vector2(_rb.velocity.x,9f);
     }
 
     void Update()
     {
+        FishSwim();
+        FishRotation();
+
+    }
+
+    void FishSwim()
+    {
+         if(Input.GetMouseButtonDown(0))
+        {
+          _rb.velocity = Vector2.zero;
+          _rb.velocity = new Vector2(_rb.velocity.x,_speed); // Fish move
+        }
+    }
+
+    void FishRotation()
+    {
         
+        if(_rb.velocity.y>0)
+        {
+           if(angle<=maxAngle)
+           {
+              angle=angle+4;
+           }
+        }else if(_rb.velocity.y<-1.2)
+        {
+            if(angle>minAngle)
+            {
+                angle = angle-2;
+            }
+        }
+
+        transform.rotation=Quaternion.Euler(0,0,angle);
     }
 }
